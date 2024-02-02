414294b033ccefe96d06544ac18e5ac88502ec30
Nisala Ravihara <116428103+NisalaRV@users.noreply.github.com>
2024-02-02 16:54:05 +0530

    set navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>tudu</title>
+    @include('libraries.styles')
 </head>
 <body>
 
+    @include('components.nav')
+    @yield('content')
+
+    @include('libraries.script')
 </body>
 </html>
--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -1,1 +1,11 @@
-<h1>uhaskhj</h1>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center" >
+                <h1>Home Page</h1>
+            </div>
+        </div>
+    </div>
+@endsection
