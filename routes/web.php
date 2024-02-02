4eb814b110da49832b5411bdf84ba10154fdc25e
Nisala Ravihara <116428103+NisalaRV@users.noreply.github.com>
2024-02-02 16:09:51 +0530

    set a route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TuduController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//home
+Route::get('/',[HomeController::class,"index"]) ->name('home');
+//tudu
+Route::get('/tudu',[TuduController::class,"index"]) ->name('tudu');
